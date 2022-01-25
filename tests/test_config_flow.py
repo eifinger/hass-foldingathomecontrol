d7979d71568974098917cae6f68d7fd617575c91
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-01-25 19:51:10 +0100

    test: Add options flow and import test

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -5,7 +5,11 @@ import pytest
 from homeassistant import config_entries, data_entry_flow
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.foldingathomecontrol.const import DOMAIN
+from custom_components.foldingathomecontrol.const import (
+    CONF_READ_TIMEOUT,
+    CONF_UPDATE_RATE,
+    DOMAIN,
+)
 
 from .const import MOCK_CONFIG
 
@@ -131,3 +135,60 @@ async def test_connection_failed_config_flow(hass):
 
     assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
     assert result["errors"] == {"base": "cannot_connect"}
+
+
+# Here we simulate a successful import from configuration.yaml.
+# Note that we use the `bypass_login` fixture here because
+# we want the config flow validation to succeed during the test.
+@pytest.mark.usefixtures("bypass_login")
+async def test_import(hass):
+    """Test a successful config flow."""
+    # Initialize a config flow
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_IMPORT}
+    )
+
+    # Check that the config flow shows the user form as the first step
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "user"
+
+    # If a user were to enter `test_username` for username and `test_password`
+    # for password, it would result in this function call
+    result = await hass.config_entries.flow.async_configure(
+        result["flow_id"], user_input=MOCK_CONFIG
+    )
+
+    # Check that the config flow is complete and a new entry is created with
+    # the input data
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert result["title"] == "localhost"
+    assert result["data"] == MOCK_CONFIG
+    assert result["result"]
+
+
+@pytest.mark.usefixtures("bypass_login")
+async def test_options_flow(hass):
+    """Test the options flow."""
+    entry = MockConfigEntry(
+        domain=DOMAIN,
+        data=MOCK_CONFIG,
+    )
+    entry.add_to_hass(hass)
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+
+    result = await hass.config_entries.options.async_init(entry.entry_id)
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_FORM
+    assert result["step_id"] == "init"
+
+    result = await hass.config_entries.options.async_configure(
+        result["flow_id"],
+        user_input={CONF_UPDATE_RATE: 10},
+    )
+
+    assert result["type"] == data_entry_flow.RESULT_TYPE_CREATE_ENTRY
+    assert entry.options == {
+        CONF_UPDATE_RATE: 10,
+        CONF_READ_TIMEOUT: 15,
+    }
