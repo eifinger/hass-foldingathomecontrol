c8e35a9e55fa250b71e0b3f12f8756a7d3903cc5
Kevin Stillhammer <kevin.stillhammer@gmail.com>
2022-02-13 16:33:48 +0100

    fix: Fix removal of slots

--- tests/const.py
+++ tests/const.py
@@ -27,6 +27,25 @@ SLOTS_DATA = [
     }
 ]
 
+TWO_SLOTS_DATA = [
+    {
+        "id": "00",
+        "status": "READY",
+        "description": "cpu: 3",
+        "options": {"idle": "false"},
+        "reason": "",
+        "idle": False,
+    },
+    {
+        "id": "01",
+        "status": "READY",
+        "description": "gpu: 1",
+        "options": {"idle": "false"},
+        "reason": "",
+        "idle": False,
+    },
+]
+
 OPTIONS_DATA = {
     "allow": "0/0",
     "command-address": "0.0.0.0",
@@ -70,4 +89,65 @@ UNITS_DATA = [
         "tpf": "2 mins 20 secs",
         "basecredit": "9405",
     }
+]
+
+TWO_UNITS_DATA = [
+    {
+        "id": "00",
+        "state": "RUNNING",
+        "error": "NO_ERROR",
+        "project": 11777,
+        "run": 0,
+        "clone": 17250,
+        "gen": 0,
+        "core": "0x22",
+        "unit": "0x00000003287234c95e774a8488c5d4ea",
+        "percentdone": "72.51%",
+        "eta": "1 hours 04 mins",
+        "ppd": "359072",
+        "creditestimate": "58183",
+        "waitingon": "",
+        "nextattempt": "0.00 secs",
+        "timeremaining": "8.08 days",
+        "totalframes": 100,
+        "framesdone": 72,
+        "assigned": "2020-03-28T08: 33: 55Z",
+        "timeout": "2020-03-29T08: 33: 55Z",
+        "deadline": "2020-04-05T13: 21: 54Z",
+        "ws": "40.114.52.201",
+        "cs": "13.82.98.119",
+        "attempts": 0,
+        "slot": "00",
+        "tpf": "2 mins 20 secs",
+        "basecredit": "9405",
+    },
+    {
+        "id": "01",
+        "state": "RUNNING",
+        "error": "NO_ERROR",
+        "project": 11777,
+        "run": 0,
+        "clone": 17250,
+        "gen": 0,
+        "core": "0x22",
+        "unit": "0x00000003287234c95e774a8488c5d4ea",
+        "percentdone": "50.51%",
+        "eta": "1 hours 44 mins",
+        "ppd": "339072",
+        "creditestimate": "56183",
+        "waitingon": "",
+        "nextattempt": "0.00 secs",
+        "timeremaining": "3.08 days",
+        "totalframes": 40,
+        "framesdone": 82,
+        "assigned": "2020-03-28T08: 33: 55Z",
+        "timeout": "2020-03-29T08: 33: 55Z",
+        "deadline": "2020-04-05T13: 21: 54Z",
+        "ws": "40.114.52.201",
+        "cs": "13.82.98.119",
+        "attempts": 0,
+        "slot": "01",
+        "tpf": "2 mins 20 secs",
+        "basecredit": "9405",
+    },
 ]
